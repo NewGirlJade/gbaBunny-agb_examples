baf07bb36af22682af18edb35f5e158c026354d4
Jayda <JadelynneMiriam@gmail.com>
2026-06-25 14:52:56 -0600

    implemented fixed-point number math and made the ball's y speed change depending on where it hits the paddle

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,25 @@
 // until you declare the extern crate. `agb` provides an allocator so it will all work
 extern crate alloc;
 
-use agb::display::GraphicsFrame;
 use agb::display::object::Object;
-use agb::fixnum::{Rect, Vector2D, rect, vec2};
+use agb::display::tiled::{RegularBackground, RegularBackgroundSize, TileFormat};
+use agb::display::{GraphicsFrame, Priority};
+use agb::fixnum::{Num, Rect, Vector2D, num, rect, vec2};
 use agb::include_aseprite;
 include_aseprite!(
     mod sprites,
     "gfx/sprites.aseprite"
 );
+use agb::include_background_gfx;
+include_background_gfx!(
+    mod background,
+    PLAY_FIELD => deduplicate "gfx/background.aseprite",
+);
+
+type Fixed = Num<i32, 8>;
 
 pub struct Paddle {
-    pos: Vector2D<i32>,
+    position: Vector2D<Fixed>,
     facing: Orientation,
 }
 #[derive(PartialEq, Eq)]
@@ -28,41 +36,94 @@ pub enum Orientation {
     R,
 }
 impl Paddle {
-    pub fn new(start_x: i32, start_y: i32, start_facing: Orientation) -> Self {
+    pub fn new(start_x: Fixed, start_y: Fixed, start_facing: Orientation) -> Self {
         Self {
-            pos: vec2(start_x, start_y),
+            position: vec2(start_x, start_y),
             facing: start_facing,
         }
     }
-    pub fn set_pos(&mut self, x: i32, y: i32) {
-        self.pos = vec2(x, y);
+    pub fn set_pos(&mut self, x: Fixed, y: Fixed) {
+        self.position = vec2(x, y);
     }
-    pub fn move_by(&mut self, y: i32) {
-        self.pos.y = (self.pos.y + y).clamp(0, agb::display::HEIGHT - (16 * 3));
+    pub fn move_by(&mut self, y: Fixed) {
+        self.position.y =
+            (self.position.y + y).clamp(num!(0), num!(agb::display::HEIGHT - (16 * 3)));
     }
     pub fn show(&self, frame: &mut GraphicsFrame) {
+        let sprite_position = self.position.round();
+
         let mut top = Object::new(sprites::PADDLE_END.sprite(0));
-        top.set_pos(self.pos);
+        top.set_pos(sprite_position);
         if self.facing == Orientation::R {
             top.set_hflip(true);
         }
         top.show(frame);
         let mut middle = Object::new(sprites::PADDLE_MID.sprite(0));
-        middle.set_pos(self.pos + vec2(0, 16));
+        middle.set_pos(sprite_position + vec2(0, 16));
         if self.facing == Orientation::R {
             middle.set_hflip(true);
         }
         middle.show(frame);
         let mut bottom = Object::new(sprites::PADDLE_END.sprite(0));
-        bottom.set_pos(self.pos + vec2(0, 32));
+        bottom.set_pos(sprite_position + vec2(0, 32));
         bottom.set_vflip(true);
         if self.facing == Orientation::R {
             bottom.set_hflip(true);
         }
         bottom.show(frame);
     }
-    pub fn collision_rect(&self) -> Rect<i32> {
-        rect(self.pos + vec2(2, 6), vec2(12, 16 * 3 - 6))
+    pub fn collision_rect(&self) -> Rect<Fixed> {
+        rect(
+            self.position + (vec2(num!(4), num!(6))),
+            vec2(num!(8), num!(16 * 3 - 6)),
+        )
+    }
+}
+
+pub struct Ball {
+    position: Vector2D<Fixed>,
+    velocity: Vector2D<Fixed>,
+}
+impl Ball {
+    pub fn new(position: Vector2D<Fixed>, velocity: Vector2D<Fixed>) -> Self {
+        Self { position, velocity }
+    }
+    pub fn update(&mut self, paddle_a: &Paddle, paddle_b: &Paddle) {
+        let potential_ball_position = self.position + self.velocity;
+
+        let ball_rect = rect(
+            potential_ball_position + vec2(num!(2), num!(2)),
+            vec2(num!(12), num!(12)),
+        );
+        if paddle_a.collision_rect().touches(ball_rect) {
+            self.velocity.x = num!(2);
+            let y_diff = (ball_rect.centre().y - paddle_a.collision_rect().centre().y) / 32;
+            self.velocity.y += y_diff;
+        }
+        if paddle_b.collision_rect().touches(ball_rect) {
+            self.velocity.x = num!(-2);
+            let y_diff = (ball_rect.centre().y - paddle_b.collision_rect().centre().y) / 32;
+            self.velocity.y += y_diff;
+        }
+        if potential_ball_position.x <= num!(0)
+            || potential_ball_position.x >= num!(agb::display::WIDTH - 16)
+        {
+            self.velocity.x *= -1
+        }
+        if potential_ball_position.y <= num!(0)
+            || potential_ball_position.y >= num!(agb::display::HEIGHT - 16)
+        {
+            self.velocity.y *= -1
+        }
+
+        self.position += self.velocity;
+    }
+    pub fn show(&self, frame: &mut GraphicsFrame) {
+        let sprite_position = self.position.round();
+
+        Object::new(sprites::BALL.sprite(0))
+            .set_pos(sprite_position)
+            .show(frame);
     }
 }
 
@@ -71,14 +132,20 @@ impl Paddle {
 #[agb::entry]
 fn main(mut gba: agb::Gba) -> ! {
     let mut gfx = gba.graphics.get();
+    gfx.set_background_palettes(background::PALETTES);
+    let mut bg = RegularBackground::new(
+        Priority::P3,
+        RegularBackgroundSize::Background32x32,
+        TileFormat::FourBpp, //to do eight bpp you need to include 256 after the fat arrow in the include_background_gfx macro above to tell the program you want 256 color mode.
+    );
+    bg.fill_with(&background::PLAY_FIELD);
+
     let mut button_controller = agb::input::ButtonController::new();
 
-    let mut ball = Object::new(sprites::BALL.sprite(0));
-    let mut ball_position = vec2(50, 50);
-    let mut ball_velocity = vec2(-2, -1);
+    let mut ball = Ball::new(vec2(num!(51), num!(50)), vec2(num!(-2), num!(-0.5)));
 
-    let mut paddle_a = Paddle::new(8, 8, Orientation::L);
-    let mut paddle_b = Paddle::new(240 - 16 - 8, 8, Orientation::R);
+    let mut paddle_a = Paddle::new(num!(8), num!(8), Orientation::L);
+    let mut paddle_b = Paddle::new(num!(240 - 16 - 8), num!(8), Orientation::R);
 
     loop {
         button_controller.update();
@@ -86,40 +153,23 @@ fn main(mut gba: agb::Gba) -> ! {
         if button_controller.is_pressed(agb::input::Button::A) {
             paddle_a_move *= 2;
         }
-        paddle_a.move_by(paddle_a_move);
+        paddle_a.move_by(Fixed::from(paddle_a_move) * num!(1.2));
 
-        if ball_position.y < paddle_b.pos.y + 32 {
-            paddle_b.move_by(-1);
+        if ball.position.y < paddle_b.position.y + 28 {
+            paddle_b.move_by(num!(-2)); //this number is what makes the AI easier or harder to bamboozle
         }
-        if ball_position.y > paddle_b.pos.y + 16 {
-            paddle_b.move_by(1);
-        }
-
-        let potential_ball_position = ball_position + ball_velocity;
-        let ball_rect = rect(potential_ball_position + vec2(2, 2), vec2(12, 12));
-        if paddle_a.collision_rect().touches(ball_rect) {
-            ball_velocity.x = 2;
-        }
-        if paddle_b.collision_rect().touches(ball_rect) {
-            ball_velocity.x = -2;
-        }
-        if potential_ball_position.x <= 0 || potential_ball_position.x >= agb::display::WIDTH - 16 {
-            ball_velocity.x *= -1
-        }
-        if potential_ball_position.y <= 0 || potential_ball_position.y >= agb::display::HEIGHT - 16
-        {
-            ball_velocity.y *= -1
+        if ball.position.y > paddle_b.position.y + 20 {
+            paddle_b.move_by(num!(2));
         }
 
-        ball_position += ball_velocity;
-        ball.set_pos(ball_position);
+        ball.update(&paddle_a, &paddle_b);
 
         let mut frame = gfx.frame();
 
         ball.show(&mut frame);
         paddle_a.show(&mut frame);
         paddle_b.show(&mut frame);
-
+        bg.show(&mut frame);
         frame.commit();
     }
 }
