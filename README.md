a16cafcee51d2be4eb74c3e7937e62ea78b1a822
Jayda <JadelynneMiriam@gmail.com>
2026-06-24 22:39:54 -0600

    24 June 2026 fresh start

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # AGBRS Playground
 
-This project is solely for me to mess around, break things, and learn. The biggest improvements I've made which you wouldn't get from following the [agb book](https://agbrs.dev/book) are runner modifications found in .cargo/config.toml and run_agbfix.sh
+This project is solely for me to mess around, break things, and learn. It's based on the template agb provides, with some minor tweaks.
+
+The biggest improvements I've made which you wouldn't get from following the [agb book](https://agbrs.dev/book) are runner modifications found in .cargo/config.toml and run_agbfix.sh
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@
 // until you declare the extern crate. `agb` provides an allocator so it will all work
 extern crate alloc;
 
+use agb::display::GraphicsFrame;
 use agb::include_aseprite;
 include_aseprite!(
     mod sprites,
@@ -16,6 +17,51 @@ include_aseprite!(
 );
 
 use agb::display::object::Object;
+
+pub struct Paddle {
+    x: i32,
+    y: i32,
+    facing: Orientation,
+}
+#[derive(PartialEq, Eq)]
+pub enum Orientation {
+    L,
+    R,
+}
+impl Paddle {
+    pub fn new(start_x: i32, start_y: i32, start_facing: Orientation) -> Self {
+        Self {
+            x: start_x,
+            y: start_y,
+            facing: start_facing,
+        }
+    }
+    pub fn set_pos(&mut self, x: i32, y: i32) {
+        self.x = x;
+        self.y = y;
+    }
+    pub fn show(&self, frame: &mut GraphicsFrame) {
+        let mut top = Object::new(sprites::PADDLE_END.sprite(0));
+        top.set_pos((self.x, self.y));
+        if self.facing == Orientation::R {
+            top.set_hflip(true);
+        }
+        top.show(frame);
+        let mut middle = Object::new(sprites::PADDLE_MID.sprite(0));
+        middle.set_pos((self.x, self.y + 16));
+        if self.facing == Orientation::R {
+            middle.set_hflip(true);
+        }
+        middle.show(frame);
+        let mut bottom = Object::new(sprites::PADDLE_END.sprite(0));
+        bottom.set_pos((self.x, self.y + 32));
+        bottom.set_vflip(true);
+        if self.facing == Orientation::R {
+            bottom.set_hflip(true);
+        }
+        bottom.show(frame);
+    }
+}
 
 // The main function must take 1 arguments and never returns, and must be marked with
 // the #[agb::entry] macro.
@@ -31,6 +77,10 @@ fn main(mut gba: agb::Gba) -> ! {
     let mut ball_y = 50;
     let mut ball_x_vel = 1;
     let mut ball_y_vel = 1;
+
+    let mut paddle_a = Paddle::new(8, 8);
+    let mut paddle_b = Paddle::new(240 - 16 - 8, 8);
+
     loop {
         ball_x = (ball_x + ball_x_vel).clamp(0, agb::display::WIDTH - 16);
 
